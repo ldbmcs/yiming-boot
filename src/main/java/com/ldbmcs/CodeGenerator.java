0581c9858a68bb4bcb2495117830a5e3afa875a4
ldbmcs <ldbmcs@gmail.com>
2020-05-13 15:12:36 +0800

    修改代码生成器

--- src/main/java/com/ldbmcs/CodeGenerator.java
+++ src/main/java/com/ldbmcs/CodeGenerator.java
@@ -62,7 +62,7 @@ public class CodeGenerator {
         // 包配置
         PackageConfig pc = new PackageConfig();
         pc.setModuleName(scanner("模块名"));
-        pc.setParent("com.ldbmcs");
+        pc.setParent("com.ldbmcs.modules");
         mpg.setPackageInfo(pc);
 
         // 自定义配置
